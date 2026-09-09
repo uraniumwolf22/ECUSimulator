1eb55491581599351e1dac6c9a23ac9632976394
uraniumwolf22 <logo3102@gmail.com>
2026-09-08 19:04:56 -0700

    added idle control loop

--- DynamicsEngine/EngineDynamics.py
+++ DynamicsEngine/EngineDynamics.py
@@ -7,6 +7,7 @@ from MAPFunctions import *
 from EngineTunables import *
 from engineStruct import Engine
 from multiprocessing import shared_memory
+import random
 
 SIMULATOR_DIR = Path(__file__).resolve().parent
 ECU_EMULATOR_PATH = SIMULATOR_DIR / "ECU"
@@ -35,6 +36,30 @@ torque_percent  = [75, 80, 85, 88,
                    100, 100, 97, 92,
                    86, 78, 69, 60]
 
+
+def simulate_wideband_o2(target_afr, current_tps, last_tps, current_rpm, current_afr_reading, time_step):
+
+    noise = random.gauss(0, 0.05)
+    
+    delta_tps = current_tps - last_tps
+
+    transient_effect = delta_tps * 0.08 
+    
+    instant_exhaust_afr = target_afr + transient_effect + noise
+    
+    clamped_rpm = max(current_rpm, 100)
+    
+    base_response = 0.5
+
+    response_rate = base_response * (clamped_rpm / 3000.0) * time_step
+    
+    alpha = min(1.0, max(0.01, response_rate))
+    
+    new_afr_reading = current_afr_reading + (instant_exhaust_afr - current_afr_reading) * alpha
+    
+    return new_afr_reading
+
+
 def main():
     time.sleep(0.1)  # Delay for process's to init
 
@@ -44,6 +69,10 @@ def main():
 
     currentRPM = 0
 
+    last_tps = engineStatus.TPS
+    simulated_afr = engineStatus.AFR_TARGET
+
+    idlectl = 0
     try:
         while 1:
             time.sleep(timeStep)
@@ -57,6 +86,8 @@ def main():
             IAT = engineStatus.IAT
             butterflyPercentOpen = (currentTPS / 100.0) ** 2
             throttleArea = butterflyPercentOpen * throttleBodySize * dischargeCoeff
+
+            currentAFRT = engineStatus.AFR_TARGET
 
             ######## RPM PHYSICS ########
 
@@ -76,7 +107,35 @@ def main():
             
             # Clamp MAP to atmosphere
             engineStatus.MAP = min(int(calculated_map / 1000), int(atmosphericPressure / 1000))
+
+            ######## AFR NOISE ########
+            simulated_afr = simulate_wideband_o2(
+                            target_afr=currentAFRT, 
+                            current_tps=currentTPS, 
+                            last_tps=last_tps, 
+                            current_rpm=currentRPM, 
+                            current_afr_reading=simulated_afr,
+                            time_step=timeStep
+                        )
             
+            engineStatus.REALAFR = simulated_afr
+            
+            # Update state for next loop
+            last_tps = currentTPS
+
+            ######## IDLE CONTROL LOOP ########
+            if engineStatus.RPM < idleRPM + 100:
+                if engineStatus.RPM < idleRPM and idlectl < 100:
+                    idlectl = idlectl + 1
+                    engineStatus.TPS = int(idlectl / 10)
+
+                if engineStatus.RPM > idleRPM and idlectl > -100:
+                    idlectl = idlectl - 1
+                    engineStatus.TPS = int(idlectl / 10)
+            
+
+            
+
             sem.release()
 
     finally:                # * Cleanly close the SHM and SEM
--- DynamicsEngine/EngineTunables.py
+++ DynamicsEngine/EngineTunables.py
@@ -8,7 +8,7 @@ throttleBodySize    = 0.00314       # Size of throttle body in M^2
 manifoldVolume      = 2.6           # manifold volume in L
 cylinderVolume      = 4             # Engine volume in L
 peakTorque          = 310.0         # Peak torque in nm
-
+idleRPM             = 500
 ######## FLYWHEEL ########
 flywheelMass        = 18.0          # mass in Kg
 flywheelRadius      = 0.18          # radius in m
@@ -18,5 +18,6 @@ staticDrag          = 15.0          # Static drag in nm
 linearDrag          = 0.005         # Linear drag in nm
 quadraticDrag       = 0.000005      # Quadratic drag in nm
 engineLoad          = 0             # Static engine load in nm
+
 ######## PHYSICS ENGINE ########
 timeStep = 0.01
